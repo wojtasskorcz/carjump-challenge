b58cfffc7b23eece76e440236df01c3e5b75de16
Wojciech Grajewski <wojtasskorcz@gmail.com>
2016-12-11 16:01:03 +0100

    Enabled stopping the web server without killing SBT

--- src/main/scala/me/carjump/fetcher/AppConfig.scala
+++ src/main/scala/me/carjump/fetcher/AppConfig.scala
@@ -6,6 +6,7 @@ import me.carjump.fetcher.actors.{CacheActor, FetcherActor}
 import me.carjump.fetcher.services.{RleCompressor, WebServer}
 
 import scala.concurrent.duration._
+import scala.io.StdIn
 
 object AppConfig extends App {
   private val compressor = new RleCompressor
@@ -13,8 +14,12 @@ object AppConfig extends App {
   private implicit val system = ActorSystem()
   private val cache = system.actorOf(CacheActor.props(compressor), "cache")
   private val fetcher = system.actorOf(FetcherActor.props(cache), "fetcher")
-  new WebServer(cache)
 
   private implicit val ec = system.dispatcher
   system.scheduler.schedule(1.second, AppProps.fetchInterval, fetcher, Fetch)
+
+  val bindingFuture = new WebServer(cache).run()
+  println(s"Server online at http://${AppProps.httpHost}:${AppProps.httpPort}/\nPress ENTER to stop...")
+  StdIn.readLine()
+  bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
 }
--- src/main/scala/me/carjump/fetcher/services/WebServer.scala
+++ src/main/scala/me/carjump/fetcher/services/WebServer.scala
@@ -3,6 +3,7 @@ package me.carjump.fetcher.services
 import akka.pattern.ask
 import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
@@ -10,14 +11,15 @@ import akka.util.Timeout
 import me.carjump.fetcher.AppProps
 import me.carjump.fetcher.actors.CacheActor.GetElement
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 import scala.concurrent.duration._
 
 class WebServer(cache: ActorRef)(implicit system: ActorSystem) {
-  implicit val materializer = ActorMaterializer()
-  implicit val askTimeout = Timeout(5.seconds)
+  private implicit val materializer = ActorMaterializer()
+  private implicit val askTimeout = Timeout(5.seconds)
 
-  val route =
+  private val route =
     get {
       path(IntNumber) { index =>
         val future = (cache ? GetElement(index)).mapTo[Option[String]]
@@ -31,6 +33,6 @@ class WebServer(cache: ActorRef)(implicit system: ActorSystem) {
       }
     }
 
-  Http().bindAndHandle(route, AppProps.httpHost, AppProps.httpPort)
+  def run(): Future[ServerBinding] = Http().bindAndHandle(route, AppProps.httpHost, AppProps.httpPort)
 
 }
